edf3d43935f43989d76bfaf356d9106bae053031
temitayo65 <iloritemitayo65@gmail.com>
2022-09-13 09:47:27 +0100

    Initial Commit

---
+++ Remodel2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Remodel2
+//
+//  Created by ADMIN on 13/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
